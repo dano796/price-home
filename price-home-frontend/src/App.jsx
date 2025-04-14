50bf0d58652cef55ec70c2df6b14df94e112bb46
Daniel Ortiz <daniel.ortiza.col@upb.edu.co>
2025-04-14 17:18:24 -0500

    update project

---
+++ price-home-frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import AboutUs from "./components/AboutUs";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import FormSection from "./components/FormSection";
+
+export const App = () => {
+  return (
+    <div className="overflow-x-hidden text-black antialiased selection:bg-gray-300 selection:text-black">
+      <div className="font-sans min-h-screen flex flex-col">
+        <Navbar />
+        <AboutUs />
+        <FormSection />
+        <Footer />   
+      </div>
+    </div>
+  );
+};
+
+export default App;
